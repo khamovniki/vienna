9da8c859f9055f3cf779a03403f11037769ddfda
Александр Тарасов <tarasovae97@mail.ru>
2018-10-27 23:20:18 +0300

    fixes

--- src/main/java/com/khamovniki/vienna/storage/scheduler/PostMessageExecutor.java
+++ src/main/java/com/khamovniki/vienna/storage/scheduler/PostMessageExecutor.java
@@ -7,9 +7,11 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import org.hibernate.Hibernate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 import com.khamovniki.vienna.storage.dto.TelegramPostRequestDto;
@@ -29,21 +31,22 @@ public class PostMessageExecutor {
     private final TagRepository tagRepository;
     private final RestTemplate botRestTemplate;
 
-    @Scheduled(fixedRate = 60_000L)
+    @Scheduled(fixedRate = 5_000L)
+    @Transactional(readOnly = true)
     private void sendScheduled() {
         List<PostMessageTask> tasks = postMessageTaskRepository.findByTimestampLessThan(Instant.now());
-        tasks.stream()
-                .map(task -> {
-                    Set<Long> users = task.getTags().stream()
-                            .map(tag -> tagRepository.findById(tag).map(Tag::getUsers).orElse(Collections.emptySet()))
-                            .flatMap(Collection::stream)
-                            .map(User::getUserId)
-                            .collect(Collectors.toSet());
-                    TelegramPostRequestDto request = TelegramPostRequestDto.builder()
-                            .message(task.getMessage())
-                            .users(users)
-                            .build();
-                    return botRestTemplate.postForEntity("/sendPost", request, ResponseEntity.class);
-                });
-}
+        tasks.forEach(task -> {
+            Hibernate.initialize(task.getTags());
+            Set<Long> users = task.getTags().stream()
+                    .map(tag -> tagRepository.findById(tag).map(Tag::getUsers).orElse(Collections.emptySet()))
+                    .flatMap(Collection::stream)
+                    .map(User::getUserId)
+                    .collect(Collectors.toSet());
+            TelegramPostRequestDto request = TelegramPostRequestDto.builder()
+                    .message(task.getMessage())
+                    .users(users)
+                    .build();
+            botRestTemplate.postForEntity("/sendPost", request, ResponseEntity.class);
+        });
+    }
 }
--- src/main/java/com/khamovniki/vienna/storage/service/impl/PostServiceImpl.java
+++ src/main/java/com/khamovniki/vienna/storage/service/impl/PostServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.transaction.annotation.Transactional;
 import com.khamovniki.vienna.storage.dto.PostRequestDto;
 import com.khamovniki.vienna.storage.entity.PostMessageTask;
 import com.khamovniki.vienna.storage.entity.PostMessageTaskRepository;
-import com.khamovniki.vienna.storage.entity.UserRepository;
 import com.khamovniki.vienna.storage.service.PostService;
 
 import lombok.RequiredArgsConstructor;
@@ -16,7 +15,6 @@ import lombok.RequiredArgsConstructor;
 public class PostServiceImpl implements PostService {
 
     private final PostMessageTaskRepository postMessageTaskRepository;
-    private final UserRepository userRepository;
 
     @Override
     @Transactional

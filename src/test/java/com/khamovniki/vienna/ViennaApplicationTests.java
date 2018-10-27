fb6fdc69e96cf88d7d66f239f6758a25d28f7ed1
Александр Тарасов <tarasovae97@mail.ru>
2018-10-27 13:10:46 +0300

    initial commit

---
+++ src/test/java/com/khamovniki/vienna/ViennaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khamovniki.vienna;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ViennaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
